bcba0f8fa608029c02926e779a5ef6322cfddf3e
Thangam <thangam@paxel.co>
2023-02-06 00:01:21 +0530

    app getting crashed when click plus button(after adding 3rd library)

--- app/src/main/java/com/firstapp/muvierecktask/MainActivity.kt
+++ app/src/main/java/com/firstapp/muvierecktask/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.firstapp.muvierecktask
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.firstapp.muvierecktask.databinding.ActivityMainBinding
+import com.nvt.color.ColorPickerDialog
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +34,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.orangeButton.setOnClickListener {
             binding.imageView.background = ContextCompat.getDrawable(applicationContext, R.color.orange)
+        }
+
+        binding.imageButton5.setOnClickListener {
+
+            val colorPicker = ColorPickerDialog(
+                applicationContext,
+                Color.BLACK, // color init
+                true, // true is show alpha
+                object : ColorPickerDialog.OnColorPickerListener {
+                    override fun onCancel(dialog: ColorPickerDialog?) {
+                        // handle click button Cancel
+                    }
+
+                    override fun onOk(dialog: ColorPickerDialog?, colorPicker: Int) {
+                        binding.imageView.background = ContextCompat.getDrawable(applicationContext, colorPicker)
+                    }
+                })
+            colorPicker.show()
         }
 
     }
